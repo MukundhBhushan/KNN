4d7a4f611daed14d4febbd1b38b65418f6c3b3fb
Mukundh Bhushan <30744831+MukundhBhushan@users.noreply.github.com>
2018-05-20 12:17:25 +0530

    added more animations

--- KNN_radial.py
+++ KNN_radial.py
@@ -27,7 +27,7 @@ def draw_cicle(x,y,r):
 
 def knn(data,k=3):
 
-    x,y=int(input('enter the value of x ')),int(input('enter the value y '))
+    x,y=int(input('enter the value of x: ')),int(input('enter the value y: '))
     r=0
     pre=[[x,y]]
 
@@ -37,38 +37,47 @@ def knn(data,k=3):
     else:
 
         final = {}
-        final['dog'] = [[eucliDist(j, pre), j] for j in data['dog']]
-        final['cat'] = [[eucliDist(j, pre), j] for j in data['cat']]
+        final['dog'] = [[round(eucliDist(j, pre)), j] for j in data['dog']]
+        final['cat'] = [[round(eucliDist(j, pre)), j] for j in data['cat']]
 
         final['dog']=sorted(final['dog'])
         final['cat'] = sorted(final['cat'])
+
+        def bigpoint(x,y):
+            plt.scatter(x,y, color='r', s=200)
 
 
 
         print(final)
         __knn=min(final, key=final.get)
 
-
-        print(final[__knn][0][1])
-
-        min_point=final[__knn][0][1]
-
         def plt_s():
-            return(plt.draw())
+            return (plt.draw())
 
-        while r<=eucliDist(min_point,pre)+0.1:
-            plt.clf()
-            data_plot()
-            plt.scatter(x, y, color='r')
-            if r>eucliDist(min_point,pre):
-                plt.scatter(min_point[0], min_point[1], color='r')
+        big_point=[]
+        for i in range(k):
+            min_point = final[__knn][i][1]
+            big_point.append(min_point)
+            print(big_point)
 
-            draw_cicle(x,y,r)
-            plt_s()
-            plt.pause(0.0000005)
+            while r <= eucliDist(min_point, pre) + 0.5:
+                plt.clf()
+                data_plot()
+                plt.scatter(x, y, color='r')#ref point
+                if r > eucliDist(min_point, pre):
+                    bigpoint(min_point[0], min_point[1])
 
-            r +=0.1
+                draw_cicle(x, y, r)
+                plt_s()
+                plt.pause(0.000000001)
+
+                r += 0.5
+
+        for i in big_point:
+            bigpoint(i[0],i[1])
+            #print(i)
         plt.show(plt_s())
 
 
-knn(data)
+k=int(input('enter the value of k: '))
+knn(data,k=k)
